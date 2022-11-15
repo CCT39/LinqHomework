a0d0ec1efe434202ff304d8a99a59936021bc9f5
CCT39 <kyoto31205@gmail.com>
2022-11-15 23:24:58 +0800

    加入專案檔案。

---
+++ LinqHomework/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //影片資料集合
+            List<Video> videoList = new List<Video>() {
+                new Video() { Name = "天竺鼠車車", Country = "日本", Duration = 2.6, Type = "動漫" },
+                new Video() { Name = "鬼滅之刃", Country = "日本", Duration = 25, Type = "動漫" },
+                new Video() { Name = "鬼滅之刃-無限列車", Country = "日本", Duration = 100, Type = "電影" },
+                new Video() { Name = "甜蜜家園SweetHome", Country = "韓國", Duration = 50, Type = "影集" },
+                new Video() { Name = "The 100 地球百子", Country = "歐美", Duration = 48, Type = "影集" },
+                new Video() { Name = "冰與火之歌Game of thrones", Country = "歐美", Duration = 60, Type = "影集" },
+                new Video() { Name = "半澤直樹", Country = "日本", Duration = 40, Type = "影集" },
+                new Video() { Name = "古魯家族：新石代", Country = "歐美", Duration = 90, Type = "電影" },
+                new Video() { Name = "角落小夥伴電影版：魔法繪本裡的新朋友", Country = "日本", Duration = 77, Type = "電影" },
+                new Video() { Name = "TENET天能", Country = "歐美", Duration = 80, Type = "電影" },
+                new Video() { Name = "倚天屠龍記2019", Country = "中國", Duration = 58, Type = "影集" },
+                new Video() { Name = "下一站是幸福", Country = "中國", Duration = 45, Type = "影集" },
+            };
+
+            //人物資料集合
+            List<Person> personList = new List<Person>()
+            {
+                new Person() { Name = "Bill", CountryPrefer = new List<string>() { "中國", "日本" }, TypePrefer = new List<string>() { "影集" } },
+                new Person() { Name = "Jimmy", CountryPrefer = new List<string>() { "日本" }, TypePrefer = new List<string>() { "動漫", "電影" } },
+                new Person() { Name = "Andy", CountryPrefer = new List<string>() { "歐美", "日本" }, TypePrefer = new List<string>() { "影集", "電影" } },
+            };
+
+            // 1. 找出所有日本的影片名稱
+            Console.WriteLine($"{Environment.NewLine}Q: 找出所有日本的影片名稱");
+            // ===================<Q1作答區>===================
+
+            var a1 = videoList.Where(x => x.Country == "日本");
+            foreach (var item in a1)
+                Console.WriteLine(item.Name);
+
+            // ===================</Q1作答區>===================
+            // 2. 找出所有歐美的影片且類型為"影集"的影片名稱
+            Console.WriteLine($"{Environment.NewLine}Q: 找出所有歐美的影片且類型為'影集'的影片名稱");
+            // ===================<Q2作答區>===================
+
+            var a2 = videoList.Where(x => x.Country == "歐美").Where(x => x.Type == "影集");
+            foreach (var item in a2)
+                Console.WriteLine(item.Name);
+
+            // ===================</Q2作答區>===================
+            // 3. 是否有影片片長超過120分鐘的影片
+            Console.WriteLine($"{Environment.NewLine}Q: 是否有影片片長超過120分鐘的影片");
+            // ===================<Q3作答區>===================
+
+            int target3 = 120;
+            var a3 = videoList.Any(x => x.Duration > target3);
+            if (a3)
+                Console.WriteLine($"存在片長超過 {target3} 分鐘的影片。");
+            else
+                Console.WriteLine($"找不到片長超過 {target3} 分鐘的影片。");
+
+            // ===================</Q3作答區>===================
+            // 4. 請列出所有人的名稱，並且用大寫英文表示，ex: Bill -> BILL
+            Console.WriteLine($"{Environment.NewLine}Q: 請列出所有人的名稱，並且用大寫英文表示");
+            // ===================<Q4作答區>===================
+
+            foreach (var a4 in personList)
+                Console.WriteLine(a4.Name.ToUpper());
+
+            // ===================</Q4作答區>===================
+            // 5. 將所有影片用片長排序(最長的在前)，並顯示排序過的排名以及片名，ex: No1: 天竺鼠車車
+            Console.WriteLine($"{Environment.NewLine}Q: 將所有影片用片長排序(最長的在前)，並顯示排序過的排名以及片名");
+            // ===================<Q5作答區>===================
+
+            var a5 = videoList.OrderBy(x => x.Duration);
+            int index5 = 0;
+            foreach (var item in a5)
+            {
+                Console.WriteLine($"No. {index5}：\t {item.Name}");
+                index5++;
+            }
+
+            // ===================</Q5作答區>===================
+            // 6. 將所有影片進行以"類型"分類，並顯示以下樣式(注意縮排)
+            /* 
+            動漫:
+                天竺鼠車車
+                鬼滅之刃
+            */
+            Console.WriteLine($"{Environment.NewLine}Q: 將所有影片進行以'類型'分類");
+            // ===================<Q6作答區>===================
+
+            var a6 = videoList.GroupBy(x => x.Type);
+            foreach (var items in a6)
+            {
+                Console.WriteLine($"{items.Key}：");
+                foreach (var item in items)
+                    Console.WriteLine($"\t{item.Name}");
+            }
+
+            // ===================</Q6作答區>===================
+            // 7. 找到第一個喜歡歐美影片的人
+            Console.WriteLine($"{Environment.NewLine}Q: 找到第一個喜歡歐美影片的人");
+            // ===================<Q7作答區>===================
+
+            string target7 = "歐美";
+            var a7 = personList.FirstOrDefault(x => x.CountryPrefer.Contains(target7));
+            if (a7 != null)
+                Console.WriteLine($"第一個喜歡 {target7} 影片的人是 {a7.Name}。");
+            else
+                Console.WriteLine($"找不到喜歡 {target7} 影片的人。");
+
+            // ===================</Q7作答區>===================
+            // 8. 找到每個人喜歡的影片(根據國家以及類型)，ex: Bill: 天竺鼠車車, 倚天屠龍記2019
+            Console.WriteLine($"{Environment.NewLine}Q: 找到每個人喜歡的影片");
+            // ===================<Q8作答區>===================
+
+            foreach (var person in personList)
+            {
+                IEnumerable<Video> favo8 = new List<Video>();
+                IEnumerable<string> personalFavos = new List<string>();
+
+                foreach (var country in person.CountryPrefer)
+                {
+                    foreach (var type in person.TypePrefer)
+                    {
+                        favo8 = favo8.Union(videoList.Where(x => x.Country == country && x.Type == type));
+                        personalFavos = favo8.Select(x => x.Name);
+                    }
+                }
+
+                var a8 = string.Join("、", personalFavos.Distinct());
+
+                Console.WriteLine($"{person.Name} 喜歡的影片有：{a8}。");
+            }
+
+            // ===================</Q8作答區>===================
+            // 9. 列出所有類型的影片總時長，ex: 動漫: 100min
+            Console.WriteLine($"{Environment.NewLine}Q: 列出所有類型的影片總時長");
+            // ===================<Q9作答區>===================
+
+            var allVideos = videoList.GroupBy(x => x.Type);
+            foreach (var group in allVideos)
+            {
+                double a9 = 0;
+                
+                Console.Write($"{group.Key}：總時長 ");
+                foreach (var item in group)
+                    a9 += item.Duration;
+                Console.WriteLine($"{a9:N1} 分鐘");
+            }
+
+            // ===================</Q9作答區>===================
+            // 10. 列出所有國家出產的影片數量，ex: 日本: 3部
+            Console.WriteLine($"{Environment.NewLine}Q: 列出所有國家出產的影片數量");
+            // ===================<Q10作答區>===================
+
+            var a10 = videoList.GroupBy(x => x.Country);
+            foreach (var group in a10)           
+                Console.WriteLine($"{group.Key}：總共有 {group.Count()} 部影片");
+
+            // ===================</Q10作答區>===================
+            Console.ReadLine();
+        }
+    }
+}
